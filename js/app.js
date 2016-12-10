f77ec2754a1f97bd967e18be516ca2617bd02642
Tushar Nimje <tusharnimje21@gmail.com>
2016-12-10 18:10:50 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,38 @@
+(function (angular, document) {
+    'use strict';
+    var config = {
+        idleTime: 1800000
+    };
+    angular.module('ngIdleDemo', ['ngIdle'])
+        .config(["idleProvider", function (idleProvider) {
+            idleProvider.idle(config.idleTime);
+        }])
+        .controller('idleDemoCtrl', ['$scope', 'idle', '$rootScope', function ($scope, idle, $rootScope) {
+
+            $scope.idleTime = config.idleTime;
+            //watch for idle
+            $scope.watch = function () {
+                idle.watch();
+            };
+
+            //unwatch idle
+            $scope.unwatch = function () {
+                idle.unwatch();
+            };
+
+            //destory idle timer
+            $scope.destroy = function () {
+                idle.destroy();
+            };
+
+            //listener for idle complete event
+            $rootScope.$on('idleComplete', function () {
+                $scope.idleCompleteMessage = "Application was idle for long time";
+            });
+        }]);
+
+
+    angular.element(document).ready(function () {
+        angular.bootstrap(document, ["ngIdleDemo"]);
+    });
+}(angular, document));
