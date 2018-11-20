cfa2c8bfbab5665e096184ea897c5f45f9fcd711
tusharnimje21@gmail.com <tusharnimje21@gmail.com>
2018-11-20 16:30:33 +0530

    fixed codacy issues and updated README.md badge

--- js/app.js
+++ js/app.js
@@ -1,38 +1,45 @@
-(function (angular, document) {
-    'use strict';
-    var config = {
-        idleTime: 1800000
-    };
-    angular.module('ngIdleDemo', ['ngIdle'])
-        .config(["idleProvider", function (idleProvider) {
-            idleProvider.idle(config.idleTime);
-        }])
-        .controller('idleDemoCtrl', ['$scope', 'idle', '$rootScope', function ($scope, idle, $rootScope) {
+(function(angular, document) {
+  "use strict";
+  var config = {
+    idleTime: 1800000
+  };
+  angular
+    .module("ngIdleDemo", ["ngIdle"])
+    .config([
+      "idleProvider",
+      function(idleProvider) {
+        idleProvider.idle(config.idleTime);
+      }
+    ])
+    .controller("idleDemoCtrl", [
+      "$scope",
+      "idle",
+      "$rootScope",
+      function($scope, idle, $rootScope) {
+        $scope.idleTime = config.idleTime;
+        //watch for idle
+        $scope.watch = function() {
+          idle.watch();
+        };
 
-            $scope.idleTime = config.idleTime;
-            //watch for idle
-            $scope.watch = function () {
-                idle.watch();
-            };
+        //unwatch idle
+        $scope.unwatch = function() {
+          idle.unwatch();
+        };
 
-            //unwatch idle
-            $scope.unwatch = function () {
-                idle.unwatch();
-            };
+        //destory idle timer
+        $scope.destroy = function() {
+          idle.destroy();
+        };
 
-            //destory idle timer
-            $scope.destroy = function () {
-                idle.destroy();
-            };
+        //listener for idle complete event
+        $rootScope.$on("idleComplete", function() {
+          $scope.idleCompleteMessage = "Application was idle for long time";
+        });
+      }
+    ]);
 
-            //listener for idle complete event
-            $rootScope.$on('idleComplete', function () {
-                $scope.idleCompleteMessage = "Application was idle for long time";
-            });
-        }]);
-
-
-    angular.element(document).ready(function () {
-        angular.bootstrap(document, ["ngIdleDemo"]);
-    });
-}(window.angular, document));
+  angular.element(document).ready(function() {
+    angular.bootstrap(document, ["ngIdleDemo"]);
+  });
+})(window.angular, document);
--- js/lib/angular-idle.js
+++ js/lib/angular-idle.js
@@ -1,130 +1,131 @@
 /*
-* @author Tushar
-* @version 1.0
-* angular idle library which can be injected in your angular application.
-* This library is used to detect if application is idle.
-*/
-(function (angular) {
-    'use strict';
-    angular.module('ngIdle', ['ngIdle.idle']);
+ * @author Tushar
+ * @version 1.0
+ * angular idle library which can be injected in your angular application.
+ * This library is used to detect if application is idle.
+ */
+(function(angular) {
+  "use strict";
+  angular.module("ngIdle", ["ngIdle.idle"]);
 
-    angular.module('ngIdle.idle', [])
-        .provider('idle', function () {
-            var options = {
-                idle: 1800000, // (default is 30min)
-                autoResume: true, // lets events automatically resume (unsets idle state/resets warning)
-                interrupt: 'mousemove keydown DOMMouseScroll mousewheel mousedown touchstart touchmove scroll',
-                isTitleDisabled: false, // disable changes in document's title
-                title: "Session has expired",
-                autoRefreshTitle: false,
-                titleRefreshTimer: 300000// (default is 5min)
-            };
+  angular.module("ngIdle.idle", []).provider("idle", function() {
+    var options = {
+      idle: 1800000, // (default is 30min)
+      autoResume: true, // lets events automatically resume (unsets idle state/resets warning)
+      interrupt:
+        "mousemove keydown DOMMouseScroll mousewheel mousedown touchstart touchmove scroll",
+      isTitleDisabled: false, // disable changes in document's title
+      title: "Session has expired",
+      autoRefreshTitle: false,
+      titleRefreshTimer: 300000 // (default is 5min)
+    };
 
-            this.idle = function (seconds) {
-                if (seconds <= 0) throw new Error('Idle must be a value in seconds, greater than 0.');
+    this.idle = function(seconds) {
+      if (seconds <= 0)
+        throw new Error("Idle must be a value in seconds, greater than 0.");
 
-                options.idle = seconds;
-            };
+      options.idle = seconds;
+    };
 
-            this.titleDisabled = function (value) {
-                if (typeof value === "boolean") {
-                    options.isTitleDisabled = value;
-                } else {
-                    throw new Error('title disable value must be boolean');
-                }
-            };
+    this.titleDisabled = function(value) {
+      if (typeof value === "boolean") {
+        options.isTitleDisabled = value;
+      } else {
+        throw new Error("title disable value must be boolean");
+      }
+    };
 
-            this.interrupt = function (events) {
-                options.interrupt = events;
-            };
+    this.interrupt = function(events) {
+      options.interrupt = events;
+    };
 
-            this.autoResume = function (value) {
-                if (typeof value === "boolean") {
-                    options.autoResume = value;
-                } else {
-                    throw new Error('auto resume value must be boolean');
-                }
-            };
+    this.autoResume = function(value) {
+      if (typeof value === "boolean") {
+        options.autoResume = value;
+      } else {
+        throw new Error("auto resume value must be boolean");
+      }
+    };
 
-            this.title = function (value) {
-                if (typeof value === "string") {
-                    options.title = value;
-                }
-            };
+    this.title = function(value) {
+      if (typeof value === "string") {
+        options.title = value;
+      }
+    };
 
-            this.$get = ['$rootScope', '$document', '$timeout', idleProvider];
+    this.$get = ["$rootScope", "$document", "$timeout", idleProvider];
 
-            function idleProvider($rootScope, $document, $timeout) {
-                var state = {
-                        idle: null,
-                        isRunning: false
-                    },
-                    idleTimer,
-                    ngip = {
-                        watch: function () {
-                            state.isRunning = true;
-                            clearTimer();
-                            toggleDocTitle(false);
-                            idleTimer = $timeout(idleComplete, options.idle);
-                        },
-                        unwatch: function () {
-                            state.isRunning = false;
-                            toggleDocTitle(false);
-                            clearTimer();
-                        },
-                        destroy: function () {
-                            toggleDocTitle(false);
-                            $document.find('body').off(options.interrupt);
-                        }
-                    },
-                    initialDocTitle = $document[0].title;
+    function idleProvider($rootScope, $document, $timeout) {
+      var state = {
+          idle: null,
+          isRunning: false
+        },
+        idleTimer,
+        ngip = {
+          watch: function() {
+            state.isRunning = true;
+            clearTimer();
+            toggleDocTitle(false);
+            idleTimer = $timeout(idleComplete, options.idle);
+          },
+          unwatch: function() {
+            state.isRunning = false;
+            toggleDocTitle(false);
+            clearTimer();
+          },
+          destroy: function() {
+            toggleDocTitle(false);
+            $document.find("body").off(options.interrupt);
+          }
+        },
+        initialDocTitle = $document[0].title;
 
-                //reset timer and start idle time from start
-                function resetTimer() {
-                    if (!state.isRunning) return;
+      //reset timer and start idle time from start
+      function resetTimer() {
+        if (!state.isRunning) return;
 
-                    clearTimer();
-                    idleTimer = $timeout(idleComplete, options.idle);
-                }
+        clearTimer();
+        idleTimer = $timeout(idleComplete, options.idle);
+      }
 
-                //clear timer
-                function clearTimer() {
-                    $timeout.cancel(idleTimer);
-                }
+      //clear timer
+      function clearTimer() {
+        $timeout.cancel(idleTimer);
+      }
 
-                //broadcast event when idle for specified time
-                function idleComplete() {
-                    state.isRunning = false;
-                    $rootScope.$broadcast('idleComplete');
+      //broadcast event when idle for specified time
+      function idleComplete() {
+        state.isRunning = false;
+        $rootScope.$broadcast("idleComplete");
 
-                    //auto resume idle timer
-                    if (options.autoResume) ngip.watch();
+        //auto resume idle timer
+        if (options.autoResume) ngip.watch();
 
-                    if (!options.isTitleDisabled) toggleDocTitle(true);
+        if (!options.isTitleDisabled) toggleDocTitle(true);
 
-                    if (options.autoRefreshTitle) refreshTitle();
-                }
+        if (options.autoRefreshTitle) refreshTitle();
+      }
 
-                function refreshTitle() {
-                    $timeout(function () {
-                        toggleDocTitle(false);
-                    }, options.titleRefreshTimer);
-                }
+      function refreshTitle() {
+        $timeout(function() {
+          toggleDocTitle(false);
+        }, options.titleRefreshTimer);
+      }
 
-                /*
-                 * toggle document title
-                 * @param {boolean} status - change document title based on status
-                 */
-                function toggleDocTitle(status) {
-                    $document[0].title = status ? options.title : initialDocTitle;
-                }
+      /*
+       * toggle document title
+       * @param {boolean} status - change document title based on status
+       */
+      function toggleDocTitle(status) {
+        $document[0].title = status ? options.title : initialDocTitle;
+      }
 
-                //Adding interrupt listener
-                $document.find('body').on(options.interrupt, function () {
-                    resetTimer();
-                });
+      //Adding interrupt listener
+      $document.find("body").on(options.interrupt, function() {
+        resetTimer();
+      });
 
-                return ngip;
-            }
-        });
-}(window.angular));
+      return ngip;
+    }
+  });
+})(window.angular);
